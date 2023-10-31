ee878dfbfa8ef4a24434931ab3ab8ce2888d542e
Juan Pablo Islas <islasjuanp@gmail.com>
2023-10-31 20:32:34 -0300

    Change project name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_cli',
+    name='jpi_python_cli',
     version='0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'python_cli = src.main:main',
+            'jpi_python_cli = src.main:main',
         ],
     },
 )
